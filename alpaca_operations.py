766e2d6752d15285868c0e7c725bf06110dde08d
Ojas Polakhare <67725442+TheRealOP@users.noreply.github.com>
2024-06-06 12:17:44 -0400

    lets try that again

--- alpaca_operations.py
+++ alpaca_operations.py
@@ -5,7 +5,7 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
-from api_keys import api_key,secret_key;
+from api_keys import api_key , secret_key;
 # Set up the trading client
 client = TradingClient(api_key, secret_key, paper=True)
 
